633fefaa31a44eecebf5e3cd8e198ff0fac1527b
Sara Wieland <sj.wieland@student.han.nl>
2025-10-04 09:16:56 +0200

    triangle implementation

--- triangle.cpp
+++ triangle.cpp
@@ -1,4 +1,6 @@
 #include "triangle.hpp"
+#include <iostream>
+#include <vector>
 
 Triangle::Triangle()
 {
@@ -7,7 +9,47 @@ Triangle::Triangle()
 
 cv::Mat Triangle::findShape(cv::Mat& inputImage, cv::Mat& originalImage)
 {
-    // TODO implement triangle detection using inputImage
-    std::cout << "Triangle detection not yet implemented." << std::endl;
-    return inputImage;
+    cv::Mat outputImage = originalImage.clone();
+    cv::Mat processableImage;
+    
+    cv::GaussianBlur(inputImage, processableImage, cv::Size(5, 5), 1, 1);
+    
+    // Find contours
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(processableImage, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    
+    int triangleCount = 0;
+    
+    for (size_t i = 0; i < contours.size(); ++i)
+    {
+        // Ignore too small or too large contours
+        double area = cv::contourArea(contours[i]);
+        if (area < 100 || area > 5000) 
+            continue;
+            
+        // convert contour to polygon
+        std::vector<cv::Point> approx;
+        double epsilon = 0.04 * cv::arcLength(contours[i], true);
+        cv::approxPolyDP(contours[i], approx, epsilon, true);
+
+        // Check if the polygon is a triangle
+        if (approx.size() == 3)
+        {
+            // Calculate center
+            cv::Moments M = cv::moments(contours[i]);
+            cv::Point center(M.m10 / M.m00, M.m01 / M.m00);
+            
+            // Draw the triangle
+            cv::polylines(outputImage, approx, true, cv::Scalar(0, 255, 0), 2);
+            cv::circle(outputImage, center, 3, cv::Scalar(0, 0, 255), -1);
+            
+            // Log triangle details
+            std::cout << "Triangle " << triangleCount << ": Center = (" << center.x << ", " << center.y 
+                      << "), Area = " << static_cast<int>(area) << std::endl;
+            triangleCount++;
+        }
+    }
+    
+    return outputImage;
 }
